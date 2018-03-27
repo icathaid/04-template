bfe57e5d56c07fd1b6657bfb5247ca1cca7f207d
Ana Ortega <anaa0hh@icloud.com>
2018-03-26 19:09:00 -0700

    finished up lab

--- ana-james/scripts/article.js
+++ ana-james/scripts/article.js
@@ -16,47 +16,34 @@ Article.prototype.toHtml = function() {
   let template = $('#handlebars-template').html();
   let templateRender = Handlebars.compile(template);
   Handlebars.registerHelper('link', function(object){
-    var helperUrl = Handlebars.escapeExpression(object.url),
-      text = Handlebars.escapeExpression(object.text);
-    return new Handlebars.safeString(
-      '<a href=\'' + url + '\'>' + text + '</a>'
+    Handlebars.escapeExpression(object.url),
+    Handlebars.escapeExpression(object.text);
+    return new Handlebars.safeString.Article(
+      '<a href=\'' + object.url + '\'>' + object.text + '</a>'
     );
   });
+  this.daysAgo = parseInt((new Date() - new Date(this.publishedOn))/60/60/24/1000);
+
+  this.publishStatus = this.publishedOn ? `published ${this.daysAgo} days ago` : '(draft)';
   return templateRender(this);
 };
-
-rawData.forEach(function(articleObject) {
-  articles.push(new Article(articleObject));
-});
-articles.forEach(function(ourNewArticleObject){
-  $('article').append(ourNewArticleObject.toHtml());
-
-});
-
-
 
 // REVIEWED: If your template will use properties that aren't on the object yet, add them.
 // Since your template can't hold any JS logic, we need to execute the logic here.
 // The result is added to the object as a new property, which can then be referenced by key in the template.
 // For example, you might want to display how old a post is, or say "(draft)" if it has no publication date:
-//this.daysAgo = parseInt((new Date() - new Date(this.publishedOn))/60/60/24/1000);
 
 
-//this.publishStatus = this.publishedOn ? `published ${this.daysAgo} days ago` : '(draft)';
+// REVIEWED: The ternary operator above accomplishes this same logic.
 
-// REVIEW: The ternary operator above accomplishes this same logic.
-if(this.publishedOn) {
-  this.publishStatus = `published ${this.daysAgo} days ago`;
-} else {
-  this.publishStatus = '(draft)';
-}
 
-// TODONE?: Use the method that Handlebars gave you to return your filled-in html template for THIS article.
+// TODONE: Use the method that Handlebars gave you to return your filled-in html template for THIS article.
 // line 19
 //};
 
-// COMMENT: Why are there parentheses around "(a,b)" in the .sort() method, but not around the "articleObject" or "article" arguments in the .forEach() methods?
-// PUT YOUR RESPONSE HERE
+// COMMENTED: Why are there parentheses around "(a,b)" in the .sort() method, but not around the "articleObject" or "article" arguments in the .forEach() methods?
+// When using arrow functions, if there is more than one parameter being used in the function, then the parameters need have parantheses. Similarly when there is only one parameter, you can also use parentheses, but you dont HAVE to.
+
 rawData.sort((a,b) => {
   return (new Date(b.publishedOn)) - (new Date(a.publishedOn));
 });
